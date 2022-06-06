6d300717cb20dd3e5793db42fa225a93886cce5b
Himanshu Tiwari <38527848+ozymandias9876@users.noreply.github.com>
2022-06-06 10:14:16 +0530

    Add files via upload

---
+++ leetcode.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+from datetime import date
+import os	
+import requests
+days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+today=date.today()
+d2 = today.strftime("%B %d %Y")
+d2=days[date.today().weekday()]+" "+d2
+url="https://leetcode.com/problemset/all/"
+html_text=requests.get(url)
+soup=BeautifulSoup(html_text.content,'lxml')
+
+question=soup.find_all('a','date-value'==d2 ,class_="hover:text-label-3 dark:hover:text-dark-label-3 group flex h-8 w-8 cursor-pointer items-center justify-center rounded-full hover:bg-fill-3 dark:hover:bg-dark-fill-3",)
+new=soup.find_all('a', class_="hover:text-label-3 dark:hover:text-dark-label-3 group flex h-8 w-8 cursor-pointer items-center justify-center")
+
+
+word="/problems"
+word1="target"	
+
+x=[]
+z=[]
+problem="https://leetcode.com"
+for i in new:
+	
+	a=str(i).find(word)
+	b=str(i).find(word1)
+	x.append(str(i)[a:b])
+for i in x:
+	if "/problem" in i:
+		z.append(i)
+today_problem=problem+z[-1]
+"""print(today_problem)
+"""
+today_problem=problem+x[0]
+today_problem=today_problem[:-2]
+print(today_problem[:-2])
+	
+
---
+++ mails.py
@@ -0,0 +1,30 @@
+from mailjet_rest import Client
+from leetcode import *
+import os
+api_key = '049ae4f1e0a2b8d6e26ec15ef491b6b2'
+api_secret = 'b2c5be0108c96ef7ddfd645a07d08bf6'
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+data = {
+  'Messages': [
+    {
+      "From": {
+        "Email": "himanshutiwarihouse@gmail.com",
+        "Name": "Himanshu"
+      },
+      "To": [
+        {
+          "Email": "bhawna2907kumari@gmail.com",
+          "Name": "Bhawna Kumari"
+        }
+      ],
+      "Subject": "Greetings from Himanshu.",
+      "TextPart": "Love You Always Baba",
+      "HTMLPart": "<h1>Love You,Always </h1>""<h3>Dear Bhawna I, welcome you to <a href='https://www.linkedin.com/in/himanshu3wari/'>Himanshu</a>!</h3><br />Wish you a happy day!"
+                    "<p>Here's your daily leetcode challenge <a href={}>question</a>".format(today_problem),
+      "CustomID": "AppGettingStartedTest"
+    }
+  ]
+}
+result = mailjet.send.create(data=data)
+print(result.status_code)
+print(result.json())
